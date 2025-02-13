d2a591b46762d72ec774d963e4c4215c6493ebe4
emmamrgn <dev.emmamrgn@gmail.com>
2025-02-13 14:34:03 +0100

    css

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const skills = document.querySelectorAll('.skill');
+const skills = document.querySelectorAll('.skill');
 
-    skills.forEach(skill => {
-        skill.addEventListener('click', function() {
-            this.classList.add('animate-skill');
-            setTimeout(() => {
-                this.classList.remove('animate-skill');
-            }, 1000); // Adjust the duration to match your animation
-        });
+skills.forEach(skill => {
+    skill.addEventListener('click', function() {
+        this.classList.add('animate-skill');
+        setTimeout(() => {
+            this.classList.remove('animate-skill');
+        }, 1000); // Adjust the duration to match your animation
     });
-});
-
+})
